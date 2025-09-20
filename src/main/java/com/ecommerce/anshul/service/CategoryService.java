d1b18c512c3a2035a3d4c21a1f753027fb01ba85
asc-chauhan <anshulasc151976@gmail.com>
2025-09-20 18:32:18 +0530

    get, post, put, delete mapping and Response Entity

---
+++ src/main/java/com/ecommerce/anshul/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.ecommerce.anshul.service;
+
+import com.ecommerce.anshul.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+   List<Category> getAllCategories();
+   void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
+}
