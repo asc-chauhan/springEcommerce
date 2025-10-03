bdba146923f50588c922fbc89b358a2fc10b0baf
asc-chauhan <anshulasc151976@gmail.com>
2025-10-03 16:14:35 +0530

    pageNumber and pageSize

--- src/main/java/com/ecommerce/anshul/service/CategoryService.java
+++ src/main/java/com/ecommerce/anshul/service/CategoryService.java
@@ -6,7 +6,7 @@ import com.ecommerce.anshul.payload.CategoryResponse;
 
 
 public interface CategoryService {
-    CategoryResponse getAllCategories();
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
 
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
--- src/main/java/com/ecommerce/anshul/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/anshul/service/CategoryServiceImpl.java
@@ -8,14 +8,12 @@ import com.ecommerce.anshul.payload.CategoryResponse;
 import com.ecommerce.anshul.repositories.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -29,8 +27,11 @@ public class CategoryServiceImpl implements CategoryService{
     private ModelMapper modelMapper;
 
     @Override
-    public CategoryResponse getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
+
+        List<Category> categories = categoryPage.getContent();
         if(categories.isEmpty())
             throw new APIException("No category created till now.");
         List<CategoryDTO> categoryDTOS = categories.stream()
