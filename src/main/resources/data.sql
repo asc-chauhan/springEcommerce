16b4eb42cdd3b96bd3743759206966b1382eaec7
asc-chauhan <anshulasc151976@gmail.com>
2026-05-29 17:52:44 +0530

    complete project designed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- Map product images by product name to correct Unsplash images
+UPDATE products SET image = 'https://images.unsplash.com/photo-1551028719-00167b16eac5?w=600&h=400&fit=crop' WHERE product_name = 'Jacket';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600&h=400&fit=crop' WHERE product_name = 'Shoes';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1507842217343-583bb7270b66?w=600&h=400&fit=crop' WHERE product_name = 'Book Shelf';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd?w=600&h=400&fit=crop' WHERE product_name = 'Computer Table';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=600&h=400&fit=crop' WHERE product_name = 'headphones';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=600&h=400&fit=crop' WHERE product_name = 'Robot';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=600&h=400&fit=crop' WHERE product_name = 'sofa';
+UPDATE products SET image = 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=600&h=400&fit=crop' WHERE product_name = 'Laptop';
